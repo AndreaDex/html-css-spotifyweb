ab762f67af4c484ac84a2af6569cb5015fcd9769
AndreaDex <desiderio.andrea@outlook.it>
2021-04-21 15:45:47 +0200

    insert alert and change album subtitle with js

---
+++ assets/js/mai.js
@@ -0,0 +1,17 @@
+//Alert//
+alert("Welcome on Spotify by Boolean class#35");
+
+//getElementById//
+document.getElementById("metal").innerHTML = "Pantera - Vulgar Display of Power";
+
+//getElementById//
+document.getElementById("quiet").innerHTML = "Music from the Motion Picture";
+
+//getElementById//
+document.getElementById("split").innerHTML = "Original Motion Picture Soundtrack";
+
+//getElementById//
+document.getElementById("cure").innerHTML = "Original Soundtrack by Benjamin Wallfisch";
+
+//getElementById//
+document.getElementById("sinister").innerHTML = "Original Motion Picture Soundtrack by Christopher Young";
